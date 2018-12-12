0a2886d5f8e14d72a2e12b8db54f64be8091cb3c
logerom <logerom@mailbox.org>
2018-12-12 16:46:59 +0100

    week_balance#: kotlin databinding: update gradle + plugin + kotlin gradle plugin has to match with gradle version

--- app/src/main/java/de/logerbyte/moneyminder/cashsummary/cashadapter/ItemSummary.kt
+++ app/src/main/java/de/logerbyte/moneyminder/cashsummary/cashadapter/ItemSummary.kt
@@ -1,6 +1,5 @@
 package de.logerbyte.moneyminder.cashsummary.cashadapter
 
+import android.databinding.ObservableField
 
-class ItemSummary {
-    val price = null;
-}
+class ItemSummary(val cost: ObservableField<Int>, val saldo: ObservableField<Int>, val saving: ObservableField<Int>)
