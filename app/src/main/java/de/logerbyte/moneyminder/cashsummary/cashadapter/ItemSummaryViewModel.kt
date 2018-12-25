ea57fc53d47b39e11ef560b997705acf8e5e7d44
logerom <logerom@mailbox.org>
2018-12-25 19:05:52 +0100

    week_balance#: view model property as string for data binding

--- app/src/main/java/de/logerbyte/moneyminder/cashsummary/cashadapter/ItemSummaryViewModel.kt
+++ app/src/main/java/de/logerbyte/moneyminder/cashsummary/cashadapter/ItemSummaryViewModel.kt
@@ -2,18 +2,18 @@ package de.logerbyte.moneyminder.cashsummary.cashadapter
 
 import android.databinding.ObservableField
 
-class ItemSummaryViewModel(cost: Int) {
-    var _cost = ObservableField<Int>()
-    var cost: Int = cost
+class ItemSummaryViewModel(cost: String) {
+    var _cost = ObservableField<String>()
+    var cost: String = cost
         set(v) = _cost.set(v)
 
 
-    var _saldo = ObservableField<Int>()
-    var saldo: Int = 99
+    var _saldo = ObservableField<String>()
+    var saldo: String = "99"
         set(value) = _saldo.set(value)
 
 
-    var _saving = ObservableField<Int>()
-    var saving = 999
+    var _saving = ObservableField<String>()
+    var saving = "999"
         set(value) = _saving.set(value)
 }
