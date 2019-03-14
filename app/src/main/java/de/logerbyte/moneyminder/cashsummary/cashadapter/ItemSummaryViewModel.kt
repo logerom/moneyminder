7c3e4358bc06c2591079c0d55d0cb78026b95580
logerom <logerom@mailbox.org>
2019-03-14 21:05:24 +0100

    week_balance#: todo for weekLimit

--- app/src/main/java/de/logerbyte/moneyminder/cashsummary/cashadapter/ItemSummaryViewModel.kt
+++ app/src/main/java/de/logerbyte/moneyminder/cashsummary/cashadapter/ItemSummaryViewModel.kt
@@ -3,8 +3,13 @@ package de.logerbyte.moneyminder.cashsummary.cashadapter
 import android.databinding.ObservableField
 
 class ItemSummaryViewModel(cost: String) {
+    companion object {
+        val weekLimit = 70
+    }
+    
     var _cost = ObservableField<String>()
     var cost: String = cost
+        // TODO: 14.03.19 when set cost, create saldo: weekLimit - cost
         set(v) = _cost.set(v)
 
 
