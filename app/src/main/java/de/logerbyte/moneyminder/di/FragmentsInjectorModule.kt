2d5264f14a17b6db24c3e2339d2de218999cc2c6
LogerJoger <logerom@mailbox.org>
2020-06-02 17:43:53 +0200

    develop: dagger: injection for fragment fix

--- app/src/main/java/de/logerbyte/moneyminder/di/FragmentsInjectorModule.kt
+++ app/src/main/java/de/logerbyte/moneyminder/di/FragmentsInjectorModule.kt
@@ -3,6 +3,7 @@ package de.logerbyte.moneyminder.di
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import de.logerbyte.moneyminder.addCashDialog.AddCashDialogFragment
+import de.logerbyte.moneyminder.deleteDialog.DeleteDialogFragment
 
 @Module
 abstract class FragmentsInjectorModule {
@@ -10,5 +11,7 @@ abstract class FragmentsInjectorModule {
     @ContributesAndroidInjector
     abstract fun contributesAddCashDialogFragment(): AddCashDialogFragment
 
+    @ContributesAndroidInjector
+    abstract fun contributesDeleteDialogFragment(): DeleteDialogFragment
 
 }
