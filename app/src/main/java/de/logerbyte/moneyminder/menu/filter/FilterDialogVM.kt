d3e5fa182b03e42bd00c41c55e283db125514bcf
LogerJoger <logerom@mailbox.org>
2020-09-06 15:56:42 +0200

    filter_menu: Filter Dialog: filter item

--- app/src/main/java/de/logerbyte/moneyminder/menu/filter/FilterDialogVM.kt
+++ app/src/main/java/de/logerbyte/moneyminder/menu/filter/FilterDialogVM.kt
@@ -1,7 +1,11 @@
 package de.logerbyte.moneyminder.menu.filter
 
 import androidx.lifecycle.ViewModel
+import de.logerbyte.moneyminder.data.db.expense.ExpenseRepo
 import javax.inject.Inject
 
-class FilterDialogVM @Inject constructor(val adapter: FilterAdapter) : ViewModel()
-// TODO-SW: inject adapter
+class FilterDialogVM @Inject constructor(val expenseRepo: ExpenseRepo) : ViewModel() {
+    init {
+        // TODO-SW: use use-cases and inject it
+    }
+}
