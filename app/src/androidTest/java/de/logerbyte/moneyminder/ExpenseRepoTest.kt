7e4f10174e1cc541d828bcc06348cf73996f8a51
logerjoger <logerom@mailbox.org>
2021-03-14 13:49:13 +0100

    SharePerson - Clean: Import fix for test

--- app/src/androidTest/java/de/logerbyte/moneyminder/ExpenseRepoTest.kt
+++ app/src/androidTest/java/de/logerbyte/moneyminder/ExpenseRepoTest.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import de.logerbyte.moneyminder.domain.database.DbMigration.MIGRATION_2_3
 import de.logerbyte.moneyminder.domain.database.ExpenseDatabase
 import de.logerbyte.moneyminder.domain.database.expense.Expense
 import de.logerbyte.moneyminder.domain.database.expense.ExpenseRepo
