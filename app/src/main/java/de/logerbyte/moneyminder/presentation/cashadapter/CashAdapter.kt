e733f8e27a6ee41e8285bd789b9a9bfd8ebfb149
logerjoger <logerom@mailbox.org>
2021-03-16 22:17:00 +0100

    SharePerson - Clean: Cashes Adapter show

--- app/src/main/java/de/logerbyte/moneyminder/presentation/cashadapter/CashAdapter.kt
+++ app/src/main/java/de/logerbyte/moneyminder/presentation/cashadapter/CashAdapter.kt
@@ -4,25 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
-import de.logerbyte.moneyminder.R
 import de.logerbyte.moneyminder.data.viewItem.DayExpenseViewItem
-import de.logerbyte.moneyminder.data.viewItem.DayExpenseViewViewItem
 import de.logerbyte.moneyminder.data.viewItem.ExpenseListViewItem
 import de.logerbyte.moneyminder.data.viewItem.ExpenseListViewType
 import de.logerbyte.moneyminder.data.viewItem.SummaryMonthViewItem
-import de.logerbyte.moneyminder.presentation.dialog.dialogDelete.DeleteDialogFragment
-import de.logerbyte.moneyminder.domain.database.expense.Expense
 import de.logerbyte.moneyminder.databinding.AdapterEntryBinding
 import de.logerbyte.moneyminder.databinding.AdapterEntryPlusSummaryBinding
-import de.logerbyte.moneyminder.domain.ExpenseDataManager
-import java.text.SimpleDateFormat
-import java.util.*
-import javax.inject.Inject
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
+
 
 /**
  * Created by logerom on 28.07.18.
